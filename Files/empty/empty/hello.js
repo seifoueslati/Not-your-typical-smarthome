e9d444513f1f547975751d04cf3bdafe76d4882d
seif oueslati <47242130+seifoueslati@users.noreply.github.com>
2024-12-27 04:08:56 -0800

    .

---
+++ Files/empty/empty/hello.js
@@ -0,0 +1,11 @@
+// the hello world program
+alert("Hello, World!");
+console.log('Hello, World!');
+// the hello world program
+document.write('Hello, World!');
+document.write('Hello, World!');
+document.write('Hello, World!');
+document.write('Hello, World!');
+document.write('Hello, World!');
+document.write('Hello, World!');
+document.write('Hello, World!');
